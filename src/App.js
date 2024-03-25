50af4096118852a2f2d978c5f11033b3ceb87aa7
NITTAGANESH <nittaganeshkumar@gmail.com>
2024-03-25 12:22:05 +0530

    todoList

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import TodoList from './components/Todolist'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import backgroundImage from './download.jpeg'
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{
+      backgroundImage: `url(${backgroundImage})`,
+      // backgroundRepeat: "no-repeat",
+      backgroundSize: "100%",
+    }}>
+      <TodoList />
     </div>
   );
 }
 
 export default App;
+
